842f3065b7295e7dbea4f42813853a658b0b9705
Bijayb37 <bijayb37@gmail.com>
2020-07-19 20:52:33 +1000

    first commit

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,6 @@ export default function Home({allPostsData}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Books that i have loved</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
             <section className={utilStyles.headingMd}>…</section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
